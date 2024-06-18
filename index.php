857d67299e7be405add030317ab29eaeb8c18a87
Alexa <alexa.nexgen08@gmail.com>
2024-06-18 21:18:33 +0800

    PHP Mailer

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="send.php" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" value=""> <br>
+        <label for="subject">Subject</label>
+        <input type="text" name="subject" value=""> <br>
+        <label for="message">Message</label>
+        <input type="text" name="message" value=""> <br>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+</html>
